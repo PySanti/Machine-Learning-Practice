b6c6ff3f76dd99a6ef53601b50f510d76b75fcee
santiago <santiagodeandrade4@gmail.com>
2025-02-13 14:37:13 -0400

    Uso de joblib para almacenar modelos

--- main.py
+++ main.py
@@ -1,27 +1,14 @@
 from utils.load_data import load_data
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
-from utils.precision import precision
 from utils.basic_preprocess import basic_preprocess
+from sklearn.linear_model import LogisticRegression  
+import joblib
+
+"""
+    Mejores parámetros para Regresión Logística: {'C': 10, 'max_iter': 500, 'penalty': 'l1', 'solver': 'liblinear', 'tol': 0.0001}
+"""
 
 [X_data, Y_data] = basic_preprocess(*load_data())
-
-X_train, X_test, Y_train, Y_test = train_test_split(X_data.copy(), Y_data.copy(), test_size=0.25, shuffle=True, random_state=42)
-
-
-print("Random forest \n")
-alg = RandomForestClassifier(n_estimators=20,min_samples_split=300)
-alg.fit(X_train, Y_train)
-precision(Y_train, alg.predict(X_train), label="train")
-precision(Y_test, alg.predict(X_test), label="test")
-
-print("\n")
-
-print("Regresion logistica \n")
-alg = LogisticRegression(solver="newton-cholesky")
-alg.fit(X_train, Y_train)
-precision(Y_train, alg.predict(X_train), label="train")
-precision(Y_test, alg.predict(X_test), label="test")
-
-print("\n")
+alg = LogisticRegression(C=10, max_iter=500, penalty="l1", solver="liblinear", tol=0.0001)
+alg.fit(X_data, Y_data)
+joblib.dump(alg, "regresion_logistica.joblib")
